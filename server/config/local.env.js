630fff81f96afd2e7b6cdf9db69f8afb9702e340
Greg Millward <greg_millward@cable.comcast.com>
2014-09-10 09:41:14 -0400

    local.env.js - comments

--- server/config/local.env.js
+++ server/config/local.env.js
@@ -2,8 +2,6 @@
 
 // Environment variables that grunt will set when the server starts locally. Use for your api keys, secrets, etc.
 // You will need to set these on the server you deploy to.
-//
-// This file should not be tracked by git.
 
 module.exports = {
   SESSION_SECRET: "yoflashcardfullstack-secret",
